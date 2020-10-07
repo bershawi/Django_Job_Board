6b280b0d57d64370e6c76380d094607e6f90565e
Abdelrhman Bershawi <Bershawi@localhost.localdomain>
2020-10-07 16:59:44 +0200

    media and static folder

--- Tutor.md
+++ Tutor.md
@@ -2,3 +2,8 @@ Relations :
     - One to many   [author - posts]    Foreginkey
     - Many to many  [user - groups]
     - One to One    [user - profile]
+
+
+
+static files :[FrontEnd] images, css, js
+media files :[upload] images
--- job/models.py
+++ job/models.py
@@ -16,6 +16,11 @@ JOB_TYPE = (
     )
 
 
+def image_upload(instance,filename):
+    imagename,extension = filename.split(".")
+    return "jobs/%s.%s"%(instance.id,extension)
+
+
 class Job(models.Model):    #table
     title = models.CharField(max_length=100)    #column
     #location  =
@@ -26,6 +31,7 @@ class Job(models.Model):    #table
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
     category = models.ForeignKey('Category',on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=image_upload)
 
     def __str__(self):
         return  self.title
